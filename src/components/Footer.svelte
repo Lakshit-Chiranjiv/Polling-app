b81cf2a9ac65a62f8293aa2ec26e6b16f4c11cce
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-17 19:28:15 +0530

    Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<main>
+    <footer class="flex items-center justify-between flex-wrap bg-slate-800 p-6 rounded">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">- Created by Lakshit</span>
+        </div>
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">Polling Base</span>
+        </div>
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <span class="font-semibold text-xl tracking-tight">© {currentYear}</span>
+        </div>
+    </footer>
+</main>
