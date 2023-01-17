19aa536d63a3425ad24cf8feeb958934c3e80ca1
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-17 19:23:30 +0530

    Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    import PollingBaseLogo from '../assets/polling-base-logo.svg';
+</script>
+
+<main>
+    <header class="flex items-center justify-between flex-wrap bg-slate-800 p-6 rounded">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+            <img src={PollingBaseLogo} alt="logo" class="fill-current h-8 w-8 mr-2" />
+            <span class="font-semibold text-xl tracking-tight">Polling Base</span>
+        </div>
+    </header>
+</main>
