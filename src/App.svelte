d3b96be4a9be6a70549c227f3596496b8ff94d2d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-17 20:45:38 +0530

    Tabs component imported

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
+  import Tabs from "./components/Tabs.svelte";
+  const items = ["Live Polls", "Past Polls", "Create Poll", "About Me"];
+  let active = items[0];
 </script>
 
 <main>
   <Header/>
+  <main class="bg-gray-300 h-screen">
+    <Tabs {items} {active}/>
+  </main>
   <Footer/>
 </main>
