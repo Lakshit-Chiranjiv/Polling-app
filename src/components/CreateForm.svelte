c5310051df5244e297e76c54aa636cdeaee2075d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-21 00:43:17 +0530

    label and input styles

--- src/components/CreateForm.svelte
+++ src/components/CreateForm.svelte
@@ -6,18 +6,22 @@
     <form>
         <div class="flex justify-around items-center">
             <div>
-                <label for="pollName">Poll Name</label>
-                <input type="text" id="pollName" name="pollName" placeholder="Poll Name"/>
+                <label for="pollName" class="text-sm">Poll Name</label><br>
+                <input type="text" id="pollName" name="pollName" placeholder="Poll Name" class="border-gray-400 outline-0 border rounded text-sm h-8 p-2"/>
+                <br>
         
-                <label for="pollDescription">Poll Description</label>
-                <textarea id="pollDescription" name="pollDescription" placeholder="Poll Description"/>
+                <label for="pollDescription" class="text-sm">Poll Description</label><br>
+                <textarea id="pollDescription" name="pollDescription" placeholder="Poll Description" class="border-gray-400 outline-0 border rounded text-sm h-20 p-2"/>
+                <br>
             </div>
 
             <div>
-                <label for="pollOption1">Poll Option 1</label>
-                <input type="text" id="pollOption1" name="pollOption1" placeholder="Poll Option 1"/>
-                <label for="pollOption2">Poll Option 2</label>
-                <input type="text" id="pollOption2" name="pollOption2" placeholder="Poll Option 2"/>
+                <label for="pollOption1" class="text-sm">Poll Option 1</label><br>
+                <input type="text" id="pollOption1" name="pollOption1" placeholder="Poll Option 1" class="border-gray-400 outline-0 border rounded text-sm h-8 p-2"/>
+                <br>
+                <label for="pollOption2" class="text-sm">Poll Option 2</label><br>
+                <input type="text" id="pollOption2" name="pollOption2" placeholder="Poll Option 2" class="border-gray-400 outline-0 border rounded text-sm h-8 p-2"/>
+                <br>
         
                 <button type="button">Add Another Option</button>
             </div>
