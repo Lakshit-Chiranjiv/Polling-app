205e1545bdf8015224a20e176a399e4871587c12
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-20 16:11:36 +0530

    included form component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
   import Tabs from "./components/Tabs.svelte";
+  import CreateForm from "./components/CreateForm.svelte";
   const items = ["Live Polls", "Past Polls", "Create Poll", "About Me"];
   let active = items[0];
 
@@ -14,6 +15,7 @@
   <Header/>
   <main class="min-h-screen">
     <Tabs {items} {active} on:change={tabChange} />
+    <CreateForm/>
   </main>
   <Footer/>
 </main>
