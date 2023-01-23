c63a62c0da3d1405194206ca9cce4ab9bb89e292
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-23 20:20:46 +0530

    Simple About me demo page

---
+++ src/components/About.svelte
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+
+<main>
+    <h1>About Me</h1>
+    <!-- Simple paragraph about me -->
+    <p>
+        I am a software developer with a passion for learning new technologies and
+        building applications. I have a background in web development and
+        currently work as a software engineer at <a href="https://www.3m.com">3M</a>.
+    </p>
+    <p>
+        I am a graduate of the University of Minnesota - Twin Cities with a
+        Bachelor of Science in Computer Science. I am currently pursuing a
+        Master of Science in Computer Science at the University of Minnesota - Twin Cities.
+    </p>
+
+    <h2>Skills</h2>
+    <p>
+        I have experience with a variety of technologies and languages. I am
+        proficient in JavaScript, TypeScript, HTML, CSS, and C#. I have
+        experience with React, Vue, Angular, Node, Express, and ASP.NET. I have
+        experience with SQL and NoSQL databases. I have experience with
+        Docker, Kubernetes, and AWS.
+    </p>
+
+    <h2>Interests</h2>
+    <p>
+        I am interested in learning new technologies and building applications.
+        I am interested in learning more about machine learning and artificial
+        intelligence. I am interested in learning more about data science and
+        data visualization. I am interested in learning more about cloud
+        computing and serverless computing.
+    </p>
+
+    <h2>Contact</h2>
+    <p>
+        If you would like to contact me, please send me an email at
+        <a href="mailto:proginvolme@gmail.com">Mail Me</a>.
+    </p>
+    
+
+</main>
