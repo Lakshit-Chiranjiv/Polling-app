bac96465f48ca71e99c9c09737b1175707eebf98
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-25 17:33:07 +0530

    variables and each loop for options

--- src/components/CreateForm.svelte
+++ src/components/CreateForm.svelte
@@ -1,5 +1,11 @@
 <script>
-
+    let pollDetails = {
+        pollName: "",
+        pollDescription: "",
+        pollOptions: ['', '']
+    };
+    let pollCount = 2;
+    let prevPollOptions = [];
 </script>
 
 <main>
@@ -8,21 +14,19 @@
         <div class="flex justify-around items-start py-4 gap-12">
             <div class="flex-1">
                 <label for="pollName" class="text-sm text-white">Poll Name</label><br>
-                <input type="text" id="pollName" name="pollName" placeholder="Poll Name" class="border-gray-400 outline-0 border rounded text-sm h-12 p-2 w-full mb-4"/>
+                <input type="text" id="pollName" name="pollName" placeholder="Poll Name" class="border-gray-400 outline-0 border rounded text-sm h-12 p-2 w-full mb-4" bind:value={pollDetails.pollName}/>
                 <br>
         
                 <label for="pollDescription" class="text-sm text-white">Poll Description</label><br>
-                <textarea id="pollDescription" name="pollDescription" placeholder="Poll Description" class="border-gray-400 outline-0 border rounded text-sm h-44 p-2 w-full mb-4"/>
+                <textarea id="pollDescription" name="pollDescription" placeholder="Poll Description" class="border-gray-400 outline-0 border rounded text-sm h-44 p-2 w-full mb-4" bind:value={pollDetails.pollDescription}/>
                 <br>
             </div>
 
             <div class="flex-1">
-                <label for="pollOption1" class="text-sm text-white">Poll Option 1</label><br>
-                <input type="text" id="pollOption1" name="pollOption1" placeholder="Poll Option 1" class="border-gray-400 outline-0 border rounded text-sm h-12 p-2 w-full mb-4"/>
-                <br>
-                <label for="pollOption2" class="text-sm text-white">Poll Option 2</label><br>
-                <input type="text" id="pollOption2" name="pollOption2" placeholder="Poll Option 2" class="border-gray-400 outline-0 border rounded text-sm h-12 p-2 w-full mb-4"/>
-                <br>
+                <label for="pollOptions" class="text-sm text-white">Poll Options</label><br>
+                {#each pollDetails.pollOptions as option, i}
+                    <input type="text" id="pollOptions" name="pollOptions" placeholder="Poll Option" class="border-gray-400 outline-0 border rounded text-sm h-12 p-2 w-full mb-4" bind:value={pollDetails.pollOptions[i]}/>
+                {/each}
         
                 <button type="button" class="text-white bg-orange-500 px-4 py-2 rounded my-4 text-lg font-bold">+</button>
             </div>
