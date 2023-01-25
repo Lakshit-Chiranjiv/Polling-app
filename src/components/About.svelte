89a4dd3e6cc8684c0930956d50cc919ec56f62de
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-25 17:15:45 +0530

    about styles

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,21 +3,21 @@
 </script>
 
 <main>
-    <h1>About Me</h1>
+    <h1 class="text-3xl font-bold my-6">About Me</h1>
     <!-- Simple paragraph about me -->
-    <p>
+    <p class="text-slate-600 mb-2">
         I am a software developer with a passion for learning new technologies and
         building applications. I have a background in web development and
         currently work as a software engineer at <a href="https://www.3m.com">3M</a>.
     </p>
-    <p>
+    <p class="text-slate-600 mb-4">
         I am a graduate of the University of Minnesota - Twin Cities with a
         Bachelor of Science in Computer Science. I am currently pursuing a
         Master of Science in Computer Science at the University of Minnesota - Twin Cities.
     </p>
 
-    <h2>Skills</h2>
-    <p>
+    <h2 class="text-xl font-semibold my-4">Skills</h2>
+    <p class="text-slate-600 mb-4">
         I have experience with a variety of technologies and languages. I am
         proficient in JavaScript, TypeScript, HTML, CSS, and C#. I have
         experience with React, Vue, Angular, Node, Express, and ASP.NET. I have
@@ -25,8 +25,8 @@
         Docker, Kubernetes, and AWS.
     </p>
 
-    <h2>Interests</h2>
-    <p>
+    <h2 class="text-xl font-semibold my-4">Interests</h2>
+    <p class="text-slate-600 mb-4">
         I am interested in learning new technologies and building applications.
         I am interested in learning more about machine learning and artificial
         intelligence. I am interested in learning more about data science and
@@ -34,8 +34,8 @@
         computing and serverless computing.
     </p>
 
-    <h2>Contact</h2>
-    <p>
+    <h2 class="text-xl font-semibold my-4">Contact</h2>
+    <p class="text-slate-600 mb-4">
         If you would like to contact me, please send me an email at
         <a href="mailto:proginvolme@gmail.com">Mail Me</a>.
     </p>
