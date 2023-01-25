5a911ece5064f4c3798a19be6186ef5c4df1c7cd
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-25 17:02:49 +0530

    removed bg-white

--- src/components/Tabs.svelte
+++ src/components/Tabs.svelte
@@ -9,7 +9,7 @@
     <div class="flex justify-center gap-8 mt-12">
         {#each items as item}
             <button
-                class='bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold hover:text-white hover:bg-black focus:outline-none focus:shadow-outline transition-all'
+                class='inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold hover:text-white hover:bg-black focus:outline-none focus:shadow-outline transition-all'
                 class:text-gray-200={item === active}
                 class:bg-orange-500={item === active}
                 on:click={() => dispatch("change", item)}
