641f98520d5eb4c559e8d3268b8f88d06f4e8af0
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-28 16:54:21 +0530

    percent widths,totalvotes & left border

--- src/components/Poll.svelte
+++ src/components/Poll.svelte
@@ -1,5 +1,8 @@
 <script>
     export let poll;
+    let votes = poll.votes;
+    let totalVotes = votes.reduce((a, b) => a + b, 0);
+    let percentages = votes.map(vote => `${vote / totalVotes * 100}%`);
 </script>
 
 <main class="p-8 border-2 rounded">
@@ -7,11 +10,13 @@
     <p class="text-slate-600 mb-4">
         {poll.description}
     </p>
-    <div class="flex flex-col gap-4">
-        {#each poll.options as option}
-            <button class="bg-orange-200 text-white py-2 rounded">{option}</button>
+    <div class="flex flex-col gap-1">
+        {#each poll.options as option, i}
+            <button class="bg-orange-200 py-2 rounded border-l-emerald-400 border-l-4" style={`width: ${percentages[i]};`}>{option}</button>
+            <br>
         {/each}
     </div>
+    <p class="text-right">Total votes : {totalVotes}</p>
 </main>
 
 <style>
