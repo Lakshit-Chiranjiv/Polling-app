3130b18145dc502eedb306b30db531d3b6189fd2
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-01-28 20:38:52 +0530

    vote event caught and made changes

--- src/components/PollsList.svelte
+++ src/components/PollsList.svelte
@@ -51,11 +51,19 @@
             pollTimeLeft: 1000,
         }
     ]
+
+    const handleVote = (e) => {
+        const { index, pollId } = e.detail;
+        let copiedPolls = [...polls];
+        let upvotedPoll = copiedPolls.find(poll => poll.id === pollId);
+        upvotedPoll.votes[index]++;
+        polls = copiedPolls;
+    }
 </script>
 
 <main class="grid grid-cols-2 gap-4 my-8">
     {#each polls as poll}
-        <Poll {poll} />
+        <Poll {poll} on:vote={handleVote} />
     {/each}
 </main>
 
